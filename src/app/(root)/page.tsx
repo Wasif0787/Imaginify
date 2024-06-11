7e91836c944b952dc9e14e4350d672c9b0aec7f6
Wasif0787 <wasifhussain787@gmail.com>
2024-06-12 00:30:34 +0530

    Added clerk auth

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,18 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton afterSignOutUrl='/' />
+      </SignedIn>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Transformationspage = () => {
+    return (
+        <div>Transformationspage</div>
+    )
+}
+
+export default Transformationspage
